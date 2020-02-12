4b0c37da5d761036e0dd5b1af363f77c267301ab
Declan <decavacado@gmail.com>
2020-02-12 16:45:38 -0500

    eww

--- source/server.js
+++ source/server.js
@@ -8,5 +8,12 @@ app.get("/home",(req,res) => {
 	res.sendFile(__dirname + "/home.html")
 })
 
+app.use(express.static(__dirname + '/2048-master/style'));
+app.use(express.static(__dirname + '/2048-master/js'));
+
+app.get("/2048", function(req,res) {
+	res.sendFile(__dirname + "/2048-master/index.html")
+})
+
 app.listen(process.env.PORT || 3000)
 console.log("everthing is fine");
