8d6778555ffad9d3cccb222cff589db0d2d3c145
Declan <decavacado@gmail.com>
2020-02-12 16:05:27 -0500

    HEllo

--- source/server.js
+++ source/server.js
@@ -1,10 +1,12 @@
 const express =  require("express");
 const app = express();
+const path = require("path")
 
-
+app.use("/public", express.static("public"));
 //Routes
 app.get("/home",(req,res) => {
-	res.send("SUP");
+	res.sendFile(__dirname + "/home.html")
 })
 
 app.listen(process.env.PORT || 3000)
+console.log("everthing is fine");
