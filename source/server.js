2be82a8526a5eb15714e772e4f7456514b5d0dc5
Declan <decavacado@gmail.com>
2020-02-12 21:00:30 -0500

    Hello2

--- source/server.js
+++ source/server.js
@@ -3,17 +3,16 @@ const app = express();
 const path = require("path")
 
 app.use("/public", express.static("public"));
+
+
 //Routes
 app.get("/home",(req,res) => {
 	res.sendFile(__dirname + "/home.html")
 })
-
-app.use(express.static(__dirname + '/2048-master/style'));
-app.use(express.static(__dirname + '/2048-master/js'));
-
-app.get("/2048", function(req,res) {
-	res.sendFile(__dirname + "/2048-master/index.html")
+app.get("/carrush",(req,res) => {
+	res.sendFile(__dirname + "/carrush.html")
 })
+
 
 app.listen(process.env.PORT || 3000)
 console.log("everthing is fine");
