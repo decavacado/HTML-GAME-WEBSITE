eeaf0c08e8a2dc461f4f6866c08471f43e7b650c
Declan <decavacado@gmail.com>
2020-02-12 22:00:13 -0500

    V2

--- source/server.js
+++ source/server.js
@@ -2,7 +2,11 @@ const express =  require("express");
 const app = express();
 const path = require("path")
 
-app.use("/public", express.static("public"));
+app.use("/public", express.static('public'));
+app.use("/style", express.static("2048-master/style"));
+app.use("/js", express.static("2048-master/js"));
+
+
 
 
 //Routes
@@ -11,6 +15,9 @@ app.get("/home",(req,res) => {
 })
 app.get("/carrush",(req,res) => {
 	res.sendFile(__dirname + "/carrush.html")
+})
+app.get("/2048", function(req,res){
+	res.sendFile(__dirname + "/2048-master/index.html")
 })
 
 
